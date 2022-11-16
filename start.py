99543aa68dfcf1cb9cf18d66da5d365cccbffc3e
devcom2020 <kwp@tlen.pl>
2022-11-16 15:39:31 +0100

    sroda 15:40

--- start.py
+++ start.py
@@ -5,13 +5,18 @@
 
 import pakiet.funcdef as p
 
-print(p.square(10))
-print(p.cube(10))
+a = p.square(10)
+b = p.cube(10)
+
+with open('test.log', 'w', encoding='utf-8') as f:
+    f.write(str(a))
+    f.write(str(b))
+
+input("Wciśnij ENTER aby zakończyć...")
 
 from pakiet.funcdef import square, cube
 
 # print(square(10))
-print(cube(10))
+# print(cube(10))
 
 # from .first import godziny
-
